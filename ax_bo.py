b28f672af7fbda7a1550df19e0c055ae342170c1
Phi Nguyen <phiwe3296@gmail.com>
2025-06-26 10:50:50 -0400

    new bo algorithm using Ax

--- ax_bo.py
+++ ax_bo.py
@@ -1,21 +1,34 @@
 from ax.service.managed_loop import optimize
+import requests
 
 # STEP 1: Define assay function
-def do_assay(pH, enzyme_conc, incubation_time, buffer):
+def do_assay(pH: float, enzyme_conc: float, incubation_time: float, buffer: float):
     """
         Inputs:
             - assay parameters
         Output:
             - plate reader results
     """
-    # This function represents real-world experiment
-    pass
+    response = requests.get("http://192.168.0.101/do_assay", params={
+        "pH": pH,
+        "enzyme_conc": enzyme_conc,
+        "incubation_time": incubation_time,
+        "buffer": buffer
+    })
+    finished = False
+    while not finished:
+        response = requests.get("http://192.168.0.101/get_status")
+        if response["status"] == "true":
+            finished = True
+
+    response = requests.get("http://192.168.0.102/get_results")
+    return response["results"]
 
 # STEP 2: Define the search space
 parameters = [
     {"name": "pH", "type": "range", "bounds": [6.0, 8.0]},
     {"name": "enzyme_conc", "type": "range", "bounds": [0.01, 0.1]},  # in mM
-    {"name": "incubation_time", "type": "range", "bounds": [30, 120]},  # in minutes
+    {"name": "incubation_time", "type": "range", "bounds": [30.0, 120.0]},  # in minutes
     {"name": "buffer", "type": "choice", "values": ["PBS", "Tris", "HEPES"]},
 ]
 
